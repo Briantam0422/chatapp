b390749b23f8e033bb2cda2e82d3a1acce970d48
Briantam0422 <briantam0422@gmail.com>
2023-01-03 00:22:50 -0800

    - add connect backend websocket api

--- pkg/controllers/chat/ChatController.go
+++ pkg/controllers/chat/ChatController.go
@@ -1,7 +1,28 @@
 package chat
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+)
 
 func Connect(c *gin.Context) {
+	u := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	conn, _ := u.Upgrade(c.Writer, c.Request, nil)
 
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		serverMessage := "Server Message: " + string(p)
+		if err := conn.WriteMessage(messageType, []byte(serverMessage)); err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
