8341bc3dc10e33ec8620d89ff0e1315b4561f3ae
Briantam0422 <briantam0422@gmail.com>
2024-02-22 11:37:10 -0800

    fix error

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"chatapp/pkg/routes"
 	"github.com/gin-gonic/gin"
+	"chatapp/pkg/utils"
 )
 
 func main() {
+    utils.LoadEnv()
 	r := gin.Default()
 	// get routers
 	routes.Routers(r)
 	// serve default port 8080
-	err := r.Run(":8080")
+	err := r.Run("0.0.0.0:8080")
 	if err != nil {
 		panic(err)
 	}
