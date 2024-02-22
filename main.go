05e696000ad73190028d3b476906b30a55c62894
Briantam0422 <briantam0422@gmail.com>
2024-02-22 11:50:23 -0800

    change port

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"chatapp/pkg/routes"
 	"github.com/gin-gonic/gin"
-// 	"chatapp/pkg/utils"
+	"chatapp/pkg/utils"
 )
 
 func main() {
-//     utils.LoadEnv()
+    utils.LoadEnv()
 	r := gin.Default()
 	// get routers
 	routes.Routers(r)
 	// serve default port 8080
-	err := r.Run("0.0.0.0:8080")
+	err := r.Run(":3000")
 	if err != nil {
 		panic(err)
 	}
